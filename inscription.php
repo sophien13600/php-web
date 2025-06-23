bf329e2305177f42bef1f74d386b17bf1efba38d
Sophien <bouderbala.s@gmail.com>
2025-06-23 14:39:25 +0200

    ajout d'un formulaire

---
+++ inscription.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Inscription</title>
+</head>
+<body>
+    <h1>Page d'inscription</h1>
+        <form action="utilisateur.php" method="get">
+            <fieldset>
+                <legend>Données de connexion</legend>
+
+            <div>
+                <label for="username">Nom d'utilisateur</label>
+                <input id ="username" type="text" autocomplete="off">
+            </div>
+             <div>
+                <label for="password">Mot de passe</label>
+                <input id ="password" type="password">
+            </div>
+            </fieldset>
+            <div>
+             <div>
+                <label for="age">Age</label>
+                <input id ="age" type="number" min=0 max= 150>
+            </div>
+            <div>
+                <label for="ville">Ville</label>
+                <select id="ville">
+                    <option value="" selected disabled>Merci de choisir votre ville</option>
+                    <option value="Marseille">Marseille</option>
+                    <option value="Lyon" >Lyon</option>
+                    <option value="Paris">Paris</option>
+                </select>
+            </div>
+            <div>
+                <label for="remarque">Remarque</label>
+                <textarea id ="remarque" cols="50" rows="5" ></textarea>
+            </div>
+            <div>
+                Genre
+                <input type="radio"  name="genre">Homme
+                <input type="radio" name="genre">Femme
+            </div>
+            
+            <div>
+                Sport préféré
+                <input type="checkbox" name="sport">Football
+                <input type="checkbox" name="sport">Handball
+                <input type="checkbox" name="sport">Tennis
+            </div>
+            <button>
+                S'inscrire
+            </button>
+
+        </form>
+</body>
+</html>
