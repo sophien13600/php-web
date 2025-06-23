c6c1b472e75206957176c69b7c0b93f7c1194b52
Sophien <bouderbala.s@gmail.com>
2025-06-23 13:34:11 +0200

    ajout de bootstrap

--- index.php
+++ index.php
@@ -3,10 +3,12 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script  src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/js/bootstrap.min.js" integrity="sha384-7qAoOXltbVP82dhxHAUje59V5r2YsVfBafyUDxEdApLPmcdhBPg1DKg1ERo0BZlK" crossorigin="anonymous"></script>
+    <link defer rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/css/bootstrap.min.css">
     <title>Home page</title>
 </head>
 <body>
-    <h1>Page d'accueil</h1>
+    <h1 class= "text-primary">Page d'accueil</h1>
     <p>Bienvenue dans notre application PHP</p>
     
 </body>
