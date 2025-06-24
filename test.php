6468f4d727d9e35bd7e9048bc03b49305e6cf19f
Sophien <bouderbala.s@gmail.com>
2025-06-24 12:03:36 +0200

    utilisation de  pour utiliser REFER et vider la session

--- test.php
+++ test.php
@@ -1,5 +1,28 @@
+<?php
+session_start();
+
+$nom_c="";
+$nom_s="";
+if(isset($_COOKIE['nom'])){
+    $nom_c = $_COOKIE['nom'];
+}
+if(isset($_SESSION['nom'])){
+    $nom_s = $_SESSION['nom'];
+}
+
+
+//var_dump($_SERVER);
+if(str_contains($_SERVER['HTTP_REFERER'],'test.php')){
+
+session_unset();//supprimer toutes les variables session
+session_destroy();
+$nom_c="";
+$nom_s="";
+}
+
+?>
 <!DOCTYPE html>
-<html lang="en">
+<html lang="fr">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -7,6 +30,12 @@
 </head>
 <body>
     <h1>Cookies</h1>
-    <p>Nom: <?php echo $_COOKIE['nom']?></p>
+    <p>Nom: <?=$_COOKIE['nom']?></p>
+    <p>Nom: <?=$nom_c?></p>
+    <h2>Session</h2>
+    <p>Nom: <?= $nom_s?></p>
+    <p>
+        <a href="test.php">Vider la session</a>
+    </p>
 </body>
 </html>
--- utilisateur.php
+++ utilisateur.php
@@ -1,5 +1,5 @@
 <?php
-
+session_start(); 
 $username = "Doe";
 if ($_REQUEST['username'] != "") {
     $username = $_REQUEST['username'];
@@ -13,13 +13,15 @@ $genre = "Madame";
 $age = $_REQUEST['age'];
 $ville = $_REQUEST['ville'];
 
+
 setcookie("nom", $username,);
+$_SESSION['nom'] = $username;
 
 ?>
 
 
 <!DOCTYPE html>
-<html lang="en">
+<html lang="fr">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
